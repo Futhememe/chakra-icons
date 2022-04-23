661a06313091e34f1187f66d55b637374e491afe
Futhememe <kyoro_souza@hotmail.com>
2022-04-23 00:58:53 -0300

    🎨 update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,52 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Button, Flex, Heading, Text } from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Container } from '../components/Container'
+import { Header } from '../components/Header'
+import { colors } from '../theme'
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Head>
-        <title>Chakra UI Icons || Home</title>
-        <meta name="description" content="Create and share your icons with more speed and simplicity" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Heading fontSize='7xl'>HELLO WORLD</Heading>
-      <Text>Project generated with ❤️ by Futhememe boilerplate generator</Text>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <Head>
+          <title>Chakra UI Icons || Home</title>
+          <meta name="description" content="Create and share your icons with more speed and simplicity" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Flex 
+          flexDirection='column'
+          pr={['2rem', '10rem']}
+          pl={['2rem', '10rem']}
+          alignItems='center'
+          textAlign='center'
+        >
+          <Heading 
+            fontSize={['2.5rem', '6xl']}
+            color={colors.black[100]}
+          >
+            Create and share your icons with more <Text as='span' color={colors.green[100]}>speed and simplicity</Text>
+          </Heading>
+          <Text 
+            color={colors.gray[100]}
+            mt='1rem'
+            fontSize={['md', 'lg']}
+          >
+            Chakra UI Icons is a project to help people to create icon packages with speed and more simplicity
+          </Text>
+
+          <Button 
+            size={'lg'} 
+            bgColor={colors.green[100]}
+            color={'white'}
+            mt='2rem'
+          >
+            Get Started
+          </Button>
+        </Flex>
+      </Container>
+    </>
   )
 }
 
